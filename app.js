fe1e5151ab89b59a04e1352b75faf21e32bb3375
Colin Schlecht <schlechtcolin@yahoo.com>
2021-08-03 15:24:12 -0700

    establishes initial test fetch from /tuors

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from "express";
+import fs from "fs";
 
 const app = express();
 
+const tuors = JSON.parse(fs.readFileSync("./dev-data/data/tours-simple.json"));
 app.get("/api/v1/tuors", (req, res) => {
-    
-})
+	res.status(200).json({
+		status: "success",
+		results: tuors.length,
+		data: {
+			tuors,
+		},
+	});
+});
 
 const port = 3000;
 app.listen(port, () => {
