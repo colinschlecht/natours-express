91ab4a3688c2ccf7dba5253967209f138638cafb
Colin Schlecht <schlechtcolin@yahoo.com>
2021-08-03 15:09:10 -0700

    adds dev-data starter files

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/v1/tuors", (req, res) => {
+    
+})
+
 const port = 3000;
 app.listen(port, () => {
-	console.log(`app running on port ${port}`);
+	console.log(`app running on port ${port}...`);
 });
