6ccd31e691d0da4103aee4629d9e6328f6634b82
Colin Schlecht <schlechtcolin@yahoo.com>
2021-08-19 12:48:33 -0700

    connects mongodb and mongoose

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config({ path: "./config.env" });
 import app from "./app.js";
 
+const DB = process.env.MONGO_DB.replace("<PASSWORD>", process.env.PASSWORD);
+
+//mongoose is an object data modeling library (ODM)
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then((con) => console.log("db connected"));
+
 const port = process.env.PORT;
+
 app.listen(port, () => {
 	console.log(`app running on port ${port}...`);
 });
